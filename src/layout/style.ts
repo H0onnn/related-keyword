0bfc74bb72a37847afa6e8fcb47d6efa10c695c7
H0onnn <dukei2017@naver.com>
2023-09-06 14:30:10 +0900

    Design : 메인 페이지 레이아웃 작업

---
+++ src/layout/style.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import { colors } from '../constants/colors';
+
+export const MainPageLayout = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  height: 462px;
+  width: 100%;
+  background-color: ${colors.primary};
+`;
