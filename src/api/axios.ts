4e45aa167ad69b004dc97ac08e57dba0419d7bbd
H0onnn <dukei2017@naver.com>
2023-09-06 17:31:59 +0900

    Feat : axios base 로직 작성

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,1 +1,23 @@
 import axios from 'axios';
+
+export const axiosBase = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+axios.interceptors.response.use(
+  response => response,
+  (error: any) => {
+    if (error.response) {
+      return Promise.reject(error);
+    } else if (error.request) {
+      console.log('요청 전송 오류: 서버에 요청을 보낼 수 없습니다.');
+    } else {
+      console.log('요청 준비 오류: 요청을 처리하는 도중 오류가 발생했습니다.');
+    }
+
+    return Promise.reject(error);
+  },
+);
