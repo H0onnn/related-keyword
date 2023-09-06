353ef7414aac865b340a3d5af63b84d526a17339
H0onnn <dukei2017@naver.com>
2023-09-06 14:31:01 +0900

    Feat : router 루트 경로 렌더링 컴포넌트 수정

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,11 @@
-// routes.tsx
 import { lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
+import MainPage from './pages/MainPage';
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" Component={Home} />
+      <Route path="/" Component={MainPage} />
     </Routes>
   );
 };
