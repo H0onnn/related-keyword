6033a057a4d468cbff606c87450f17dd87a618ed
H0onnn <dukei2017@naver.com>
2023-09-06 17:30:15 +0900

    Feat : input button 컴포넌트 구현

---
+++ src/components/InputButton.tsx
@@ -0,0 +1,26 @@
+import { styled } from 'styled-components';
+import { colors } from '../constants/colors';
+import InputImage from './InputImage';
+
+const InputButton: React.FC = () => {
+  return (
+    <StyledInputButton>
+      <InputImage />
+    </StyledInputButton>
+  );
+};
+
+export default InputButton;
+
+const StyledInputButton = styled.button`
+  width: 48px;
+  height: 48px;
+  border: 0;
+  border-radius: 100%;
+  background-color: ${colors.secondary};
+  border: none;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
