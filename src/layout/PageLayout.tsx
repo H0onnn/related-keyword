a8d062895c8b08e286617e1259fc1561ac78a908
H0onnn <dukei2017@naver.com>
2023-09-06 14:31:16 +0900

    Feat : 페이지레이아웃 컴포넌트 구현

--- src/layout/PageLayout.tsx
+++ src/layout/PageLayout.tsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import { MainPageLayout } from './style';
 
-const PageLayout = () => {
-  return <div>PageLayout</div>;
+interface PageLayoutProps {
+  children: React.ReactNode;
+}
+
+const PageLayout = ({ children }: PageLayoutProps) => {
+  return (
+    <MainPageLayout>
+      <main>{children}</main>
+    </MainPageLayout>
+  );
 };
 
 export default PageLayout;
