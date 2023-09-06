612eb9f0d0708c5fd961e9b8d1de009f7e5230bb
H0onnn <dukei2017@naver.com>
2023-09-06 22:56:03 +0900

    Feat : api 요청 로직 로컬 캐싱 추가

--- src/api/data.ts
+++ src/api/data.ts
@@ -1,9 +1,19 @@
 import { axiosBase } from './axios';
 import { KeywordDataTypes } from '../constants/types';
+import localCache from '../utils/localCache';
 
-export const getKeywordData = async (query: string): Promise<KeywordDataTypes> => {
-  const response = await axiosBase.get(`data?q=${query}`);
+export const getKeywordData = async (
+  query: string,
+  cacheResponse: boolean,
+): Promise<KeywordDataTypes[]> => {
+  const response = await axiosBase.get(`?q=${query}`);
   const data = response.data;
 
+  cacheResponse && localCache.writeToCache(query, data);
+
   return data;
+};
+
+export const getCachedKeywordData = (query: string): KeywordDataTypes[] => {
+  return localCache.readFromCache(query);
 };
