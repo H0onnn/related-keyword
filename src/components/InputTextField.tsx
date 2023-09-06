21f3f77bbdde51fb8890b9fa3c960fdb249aa872
H0onnn <dukei2017@naver.com>
2023-09-06 17:30:46 +0900

    Feat : input field 컴포넌트 구현

---
+++ src/components/InputTextField.tsx
@@ -0,0 +1,39 @@
+import { styled } from 'styled-components';
+import { colors } from '../constants/colors';
+
+interface InputTextFieldProps {
+  onFocus: () => void;
+  onBlur: () => void;
+}
+
+const InputTextField = ({ onFocus, onBlur }: InputTextFieldProps) => {
+  return (
+    <StyledInput
+      placeholder="질환명을 입력해 주세요."
+      type="text"
+      onFocus={onFocus}
+      onBlur={onBlur}
+    />
+  );
+};
+
+export default InputTextField;
+
+const StyledInput = styled.input`
+  flex: 1;
+  width: 100%;
+  height: 100%;
+  border: none;
+  background-color: transparent;
+  font-size: 18px;
+  padding-right: 25px;
+
+  &:focus {
+    outline: none;
+    caret-color: ${colors.secondary};
+  }
+
+  &:focus::placeholder {
+    color: transparent;
+  }
+`;
