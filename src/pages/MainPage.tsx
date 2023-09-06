1a3938ce575f252f55f62398792e6ebdfc18748a
H0onnn <dukei2017@naver.com>
2023-09-06 16:23:40 +0900

    Feat : 메인페이지 구현

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,20 @@
+import { styled } from 'styled-components';
+import Input from '../components/Input';
+
+const MainPage: React.FC = () => {
+  return (
+    <div>
+      <MainPageComment>국내 모든 임상시험 검색하고{'\n'}온라인으로 참여하기</MainPageComment>
+      <Input />
+    </div>
+  );
+};
+
+export default MainPage;
+
+const MainPageComment = styled.h2`
+  font-size: 34px;
+  margin-bottom: 40px;
+  text-align: center;
+  white-space: pre-line;
+`;
