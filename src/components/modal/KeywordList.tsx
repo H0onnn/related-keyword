e93185d82d0762af4c43331cb89f215583723ba7
H0onnn <dukei2017@naver.com>
2023-09-07 12:49:00 +0900

    Feat : list 컴포넌트 ref 타입 및 props 추가

--- src/components/modal/KeywordList.tsx
+++ src/components/modal/KeywordList.tsx
@@ -3,13 +3,22 @@ import { KeywordDataTypes } from '../../constants/types';
 
 interface KeywordListProps {
   queries: KeywordDataTypes[];
+  selectedItem: number;
+  refs: React.MutableRefObject<HTMLDivElement[]>;
 }
 
-const KeywordList = ({ queries }: KeywordListProps) => {
+const KeywordList = ({ queries, selectedItem, refs }: KeywordListProps) => {
   return (
     <div>
-      {queries.map(query => (
-        <KeywordItem key={query.sickCd} query={query.sickNm} />
+      {queries.map((query, index) => (
+        <KeywordItem
+          key={query.sickCd}
+          query={query.sickNm}
+          isSelected={index === selectedItem}
+          ref={el => {
+            if (el) refs.current[index] = el;
+          }}
+        />
       ))}
     </div>
   );
