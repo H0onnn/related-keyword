848452e2e7a7d4341066f1e70d7ca159eccaec73
H0onnn <dukei2017@naver.com>
2023-09-18 18:05:44 +0900

    Refactor : axios client 로직 class 문법으로 수정

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,23 +1,48 @@
 import axios from 'axios';
+import { API_BASE_URL } from '../constants/url';
 
-export const axiosBase = axios.create({
-  baseURL: 'https://preonboardingapiserver.vercel.app/api/data',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-});
+class AxiosClient {
+  #instance;
+  #baseURL;
 
-axios.interceptors.response.use(
-  response => response,
-  (error: any) => {
-    if (error.response) {
-      return Promise.reject(error);
-    } else if (error.request) {
-      console.log('요청 전송 오류: 서버에 요청을 보낼 수 없습니다.');
-    } else {
-      console.log('요청 준비 오류: 요청을 처리하는 도중 오류가 발생했습니다.');
+  constructor(baseURL: string) {
+    this.#baseURL = baseURL;
+    this.#instance = axios.create({
+      baseURL: this.#baseURL,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    this.#setUpInterceptors();
+  }
+
+  #setUpInterceptors() {
+    this.#instance.interceptors.response.use(
+      response => response,
+      (error: any) => {
+        if (error.response) {
+          return Promise.reject(error);
+        } else if (error.request) {
+          console.log('요청 전송 오류: 서버에 요청을 보낼 수 없습니다.');
+        } else {
+          console.log('요청 준비 오류: 요청을 처리하는 도중 오류가 발생했습니다.');
+        }
+
+        return Promise.reject(error);
+      },
+    );
+  }
+
+  async search(query: string) {
+    try {
+      const res = await this.#instance.get(`?q=${query}`);
+      console.info('api');
+      return res.data;
+    } catch (e) {
+      console.log(e);
     }
+  }
+}
 
-    return Promise.reject(error);
-  },
-);
+export const httpClient = new AxiosClient(API_BASE_URL);
