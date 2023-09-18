752e047db70f2b02305f23ada6daf81d280b86b9
H0onnn <dukei2017@naver.com>
2023-09-18 20:48:10 +0900

    Feat : 캐싱 로직에 에러 분기처리

--- src/utils/localCache.ts
+++ src/utils/localCache.ts
@@ -1,6 +1,7 @@
 import { KeywordDataTypes } from '../constants/types';
 
-const cacheName = 'sick-cache';
+const cacheVersion = 'v1';
+const cacheName = `sick-cache-${cacheVersion}`;
 
 const localCache = (() => {
   const writeToCache = async (
@@ -8,35 +9,43 @@ const localCache = (() => {
     data: KeywordDataTypes[],
     EXPIRE_TIME: number = 5 * 60 * 1000,
   ) => {
-    const cache = await caches.open(cacheName);
-    const expired = new Date().getTime() + EXPIRE_TIME;
+    try {
+      const cache = await caches.open(cacheName);
+      const expired = new Date().getTime() + EXPIRE_TIME;
 
-    const request = new Request(key);
-    const responseData = {
-      data,
-      expired,
-    };
+      const request = new Request(key);
+      const responseData = {
+        data,
+        expired,
+      };
 
-    const response = new Response(JSON.stringify(responseData));
-
-    cache.put(request, response);
+      const response = new Response(JSON.stringify(responseData));
+      cache.put(request, response);
+    } catch (error) {
+      console.error('데이터 캐싱 중 오류가 발생했습니다:', error);
+    }
   };
 
   const readFromCache = async (key: string) => {
-    const cache = await caches.open(cacheName);
-    const response = await cache.match(key);
+    try {
+      const cache = await caches.open(cacheName);
+      const response = await cache.match(key);
 
-    if (!response) return [];
+      if (!response) return [];
 
-    const responseData = await response.json();
-    const now = new Date().getTime();
+      const responseData = await response.json();
+      const now = new Date().getTime();
 
-    if (now > responseData.expired) {
-      cache.delete(key);
+      if (now > responseData.expired) {
+        cache.delete(key);
+        return [];
+      }
+
+      return responseData.data || [];
+    } catch (error) {
+      console.error('캐싱 데이터를 읽는 도중 오류가 발생했습니다:', error);
       return [];
     }
-
-    return responseData.data || [];
   };
 
   return {
