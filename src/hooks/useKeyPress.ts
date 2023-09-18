c0f352275dc8a16c6f68b253175bd62fbbcfccc8
H0onnn <dukei2017@naver.com>
2023-09-18 19:41:22 +0900

    Refactor : 키보드 입력 로직 useKeyPress와 병합

--- src/hooks/useKeyPress.ts
+++ src/hooks/useKeyPress.ts
@@ -1,31 +1,66 @@
 import { useState, useEffect } from 'react';
+import { KeywordDataTypes } from '../constants/types';
 
-const useKeyPress = (targetKey: string) => {
-  const [keyPressed, setKeyPressed] = useState<boolean>(false);
+const useKeyPress = (
+  keywordData: KeywordDataTypes[],
+  setQuery: React.Dispatch<React.SetStateAction<string>>,
+) => {
+  const [currentItem, setCurrentItem] = useState<number>(0);
+
+  const [downPressed, setDownPressed] = useState<boolean>(false);
+  const [upPressed, setUpPressed] = useState<boolean>(false);
+  const [enterPressed, setEnterPressed] = useState<boolean>(false);
 
   useEffect(() => {
-    const downHandler = ({ key }: KeyboardEvent) => {
-      if (key === targetKey) {
-        setKeyPressed(true);
+    const downUpHandler = ({ key, type }: KeyboardEvent) => {
+      const isKeyPressed = type === 'keydown'; // keydown 이면 true, keyup 이면 false
+
+      switch (key) {
+        case 'ArrowDown':
+          setDownPressed(isKeyPressed);
+          break;
+        case 'ArrowUp':
+          setUpPressed(isKeyPressed);
+          break;
+        case 'Enter':
+          setEnterPressed(isKeyPressed);
+          break;
       }
     };
 
-    const upHandler = ({ key }: KeyboardEvent) => {
-      if (key === targetKey) {
-        setKeyPressed(false);
-      }
-    };
-
-    window.addEventListener('keydown', downHandler);
-    window.addEventListener('keyup', upHandler);
+    window.addEventListener('keydown', downUpHandler);
+    window.addEventListener('keyup', downUpHandler);
 
     return () => {
-      window.removeEventListener('keydown', downHandler);
-      window.removeEventListener('keyup', upHandler);
+      window.removeEventListener('keydown', downUpHandler);
+      window.removeEventListener('keyup', downUpHandler);
     };
-  }, [targetKey]);
+  }, []);
 
-  return keyPressed;
+  // 네비게이션 동작
+  useEffect(() => {
+    if (keywordData.length && downPressed) {
+      const nextItem = (currentItem + 1) % keywordData.length;
+      setCurrentItem(nextItem);
+    }
+  }, [downPressed]);
+
+  useEffect(() => {
+    if (keywordData.length && upPressed) {
+      const prevItem = (currentItem - 1 + keywordData.length) % keywordData.length;
+      setCurrentItem(prevItem);
+    }
+  }, [upPressed]);
+
+  useEffect(() => {
+    if (keywordData.length && enterPressed) {
+      const selectedKeyword = keywordData[currentItem]?.sickNm;
+
+      if (selectedKeyword) setQuery(selectedKeyword);
+    }
+  }, [enterPressed]);
+
+  return currentItem;
 };
 
 export default useKeyPress;
